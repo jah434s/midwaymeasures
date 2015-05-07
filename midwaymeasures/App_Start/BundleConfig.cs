fb4d7602df3b5ddae636d0836a5d10abc79452f5
jah434s <heath.josh@gmail.com>
2015-05-07 16:19:16 -0500

    changed bundleconfig.cs

--- midwaymeasures/App_Start/BundleConfig.cs
+++ midwaymeasures/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace midwaymeasures
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/css/bootstrap.css",
-                      "~/Content/css/site.css"));
+                      "~/bootstrap.css",
+                      "~/site.css"));
         }
     }
 }
